262e0b8a54c330afb7c81481fbea6ea4c1dc21ac
JesusALO <a6519150014@utch.edu.mx>
2022-03-25 14:28:12 -0700

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then( m => m.ListPageModule)
+  },
+  {
+    path: 'details',
+    loadChildren: () => import('./details/details.module').then( m => m.DetailsPageModule)
+  },
+  {
+    path: 'report',
+    loadChildren: () => import('./report/report.module').then( m => m.ReportPageModule)
+  },
+  {
+    path: 'details2',
+    loadChildren: () => import('./details2/details2.module').then( m => m.Details2PageModule)
+  },
+  {
+    path: 'details3',
+    loadChildren: () => import('./details3/details3.module').then( m => m.Details3PageModule)
+  },
+  {
+    path: 'details4',
+    loadChildren: () => import('./details4/details4.module').then( m => m.Details4PageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
